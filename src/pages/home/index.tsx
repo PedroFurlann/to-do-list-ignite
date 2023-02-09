4a683a34c2be31426e198525e7fb15b86b762fd4
Pedro Furlan <pedrofurlan6@gmail.com>
2023-02-08 23:48:50 -0300

    Create Empty tasks

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,89 @@
-import { SearchForm } from '../components/SearchForm'
-import { HomeContainer } from './styles'
+import { PlusCircle } from 'phosphor-react'
+import { FormEvent, useState } from 'react'
+// import { SearchForm } from '../components/SearchForm'
+import {
+  EmptyTableContainer,
+  HeaderTableContainer,
+  HomeContainer,
+  SearchFormContainer,
+  TableContainer,
+  TaskCreatedAndDone,
+} from './styles'
+
+import Clipboard from '../../assets/Clipboard.svg'
 
 export function Home() {
+  const [taskCreated, setTaskCreated] = useState<number | null>(null)
+  const [taskDone, setTaskDone] = useState(0)
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+
+    setTaskCreated((state) => {
+      return state! + 1
+    })
+  }
+
+  console.log(taskCreated)
+
   return (
     <HomeContainer>
-      <SearchForm />
+      <SearchFormContainer onSubmit={handleCreateNewTask}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+
+        <button type="submit">
+          Criar
+          <PlusCircle size={16} />
+        </button>
+      </SearchFormContainer>
+      <HeaderTableContainer>
+        <div>
+          <TaskCreatedAndDone variant="created">
+            Tarefas Criadas
+          </TaskCreatedAndDone>
+          <span>{taskCreated}</span>
+        </div>
+        <div>
+          <TaskCreatedAndDone variant="done">Concluídas</TaskCreatedAndDone>
+          <span>{taskDone}</span>
+        </div>
+      </HeaderTableContainer>
+
+      {taskCreated === 0 || taskCreated === null ? (
+        <div>
+          <hr />
+          <EmptyTableContainer>
+            <img src={Clipboard} alt="" />
+            <div>
+              <p id="bold">Você ainda não tem tarefas cadastradas</p>
+              <p id="no-bold">Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          </EmptyTableContainer>
+        </div>
+      ) : (
+        <TableContainer>
+          <tbody>
+            <td>
+              <tr>
+                <td>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+                  consequatur facere nulla at eveniet, in dolorem similique quas
+                  earum perspiciatis eligendi dolore labore voluptatibus
+                  provident tenetur! A fuga quisquam dolorum.
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem dolore ex quasi cum! Magnam dolor aspernatur eaque
+                  vitae ea non temporibus quasi quisquam accusamus dolore. Saepe
+                  vel sequi quisquam quaerat.
+                </td>
+              </tr>
+            </td>
+          </tbody>
+        </TableContainer>
+      )}
     </HomeContainer>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -5,10 +5,109 @@ export const HomeContainer = styled.main`
   max-width: 1120px;
   margin: 4rem auto 0;
   padding: 0 1.5rem;
+
+  hr {
+    border-color: #808080;
+    margin-bottom: 4rem;
+  }
 `
 
-export const TasksContainer = styled.div``
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 0.5rem;
+  margin-top: -5rem;
 
-export const HeaderTableContainer = styled.div``
+  input {
+    flex: 1;
+    border-radius: 8px;
+    background-color: #333333;
+    color: #fff;
+    height: 3.5rem;
+    padding: 1rem;
+    font-size: 1rem;
 
-export const TableContainer = styled.table``
+    &::placeholder {
+      color: #808080;
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    padding: 1rem;
+    background: blue;
+    color: #f2f2f2;
+    font-weight: bold;
+    border-radius: 8px;
+    cursor: pointer;
+    background-color: #1e6f9f;
+  }
+`
+
+export const EmptyTableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  div > p#bold {
+    font-size: 16px;
+    font-weight: bolder;
+    color: #808080;
+  }
+
+  div > p#no-bold {
+    font-size: 16px;
+    color: #808080;
+  }
+`
+
+export const HeaderTableContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 6rem;
+  margin-bottom: 1.5rem;
+
+  div {
+    display: flex;
+    gap: 0.5rem;
+
+    > span {
+      font-weight: bold;
+    }
+  }
+`
+
+interface TaskProps {
+  variant: 'created' | 'done'
+}
+
+export const TaskCreatedAndDone = styled.p<TaskProps>`
+  color: ${(props) => (props.variant === 'created' ? '#4ea8de' : '#8284fa')};
+`
+
+export const TableContainer = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  margin-top: 1.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: #333333;
+    margin-bottom: 10rem;
+
+    &:first-child {
+      border-top-left-radius: 8px;
+      border-bottom-left-radius: 8px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
+    }
+  }
+`
